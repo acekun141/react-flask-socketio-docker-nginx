4aa9a96f82022ca589b5160da85ab3126502c203
Ace Kun <acekun141@gmail.com>
2020-04-20 09:16:44 +0700

    first-commit

--- src/pages/DirectPage.js
+++ src/pages/DirectPage.js
@@ -1,45 +1,33 @@
 import React from 'react';
 import {Avatar} from './HomePage';
-import {FiSend} from 'react-icons/fi';
+import {FiSend, FiUserCheck, FiUserX} from 'react-icons/fi';
 
 
 export default function DirectPage() {
     return (
         <div className='module-directpage'>
             <div className='left-side'>
-                <div className='search'>
-                    <input
-                        type='text'
-                        placeholder='Find your friend'
-                    />
+                <div className='side-header'>
+                    <button className='choose friends active'>
+                        <FiUserCheck size={20} />
+                        <p>Friends</p>
+                    </button>
+                    <button className='choose unknown'>
+                        <FiUserX size={20} />
+                        <p>Unknown</p>
+                    </button>
                 </div>
-                <button className='friend'>
-                    <div className='friend-left'>
-                        <Avatar />
-                    </div>
-                    <div className='friend-right'>
-                        <p className='name'>Le Viet Hung</p>
-                        <p className='current'>Current message</p>
-                    </div>
-                </button>
-                <button className='friend'>
-                    <div className='friend-left'>
-                        <Avatar />
-                    </div>
-                    <div className='friend-right'>
-                        <p className='name'>Le Viet Hung</p>
-                        <p className='current'>Current message</p>
-                    </div>
-                </button>
-                <button className='friend'>
-                    <div className='friend-left'>
-                        <Avatar />
-                    </div>
-                    <div className='friend-right'>
-                        <p className='name'>Le Viet Hung</p>
-                        <p className='current'>Current message</p>
-                    </div>
-                </button>
+                <div className='wrap-list-friend'>
+                    <button className='friend'>
+                        <div className='friend-left'>
+                            <Avatar />
+                        </div>
+                        <div className='friend-right'>
+                            <p className='name'>Le Viet Hung</p>
+                            <p className='current'>Current message</p>
+                        </div>
+                    </button>
+                </div>
             </div>
             <div className='right-side'>
                 <div className='side-header'>
@@ -49,7 +37,16 @@ export default function DirectPage() {
                     </p>
                 </div>
                 <div className='side-content'>
-                    <div className='messages'></div>
+                    <div className='messages'>
+                        <div className='wrap-message'>
+                            <p className='message'>Test message from server</p>
+                            <p className='time'>19:38</p>
+                        </div>
+                        <div className='wrap-message your-message'>
+                            <p className='message'>Your message</p>
+                            <p className='time'>19:39</p>
+                        </div>
+                    </div>
                     <div className='typing'>
                         <form>
                             <input
@@ -57,7 +54,7 @@ export default function DirectPage() {
                                 placeholder='Message...'
                             />
                             <button>
-                                <FiSend size={20}/>
+                                <FiSend size={25}/>
                             </button>
                         </form>
                     </div>
